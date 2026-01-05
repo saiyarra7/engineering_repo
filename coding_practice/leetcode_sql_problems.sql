4cd877acf881dd289558a2ba3febe0acec95d722
saiyarra7 <38370501+saiyarra7@users.noreply.github.com>
2026-01-04 22:49:00 -0600

    620. Not Boring Movies

--- coding_practice/leetcode_sql_problems.sql
+++ coding_practice/leetcode_sql_problems.sql
@@ -1,3 +1,10 @@
+--620. Not Boring Movies
+--https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+select * from cinema
+where id%2=1 and description <> 'boring'
+order by rating desc;
+
+
 -- 1174. Immediate Food Delivery II
 -- https://leetcode.com/problems/immediate-food-delivery-ii/description/?envType=study-plan-v2&envId=top-sql-50
 
@@ -33,4 +40,36 @@ group by player_id) first_login
 left join activity a on a.player_id = first_login.player_id
 and a.event_date = first_login.first_login + interval '1 day';
 
--- Optimized using window functions
+-- Optimized using min window functions, runtime : 491 ms
+select round(sum(is_retained::int)::decimal/count(distinct player_id),2) as fraction
+from 
+(select player_id, 
+(event_date = (min(event_date) over (partition by player_id) + interval '1 day'))::int as is_retained
+from activity)
+
+-- optimized using array_agg and filter clause, runtime
+with player_stats as 
+(select player_id, min(event_date) = Any(array_agg(event_date - interval '1 day')) as qualified
+from activity
+group by player_id)
+select round(count(*) filter (where qualified)::decimal /count(player_id),2) as fraction
+from player_stats;
+
+--usine lead function
+WITH temp AS (
+    SELECT 
+        player_id, 
+        event_date,
+        DENSE_RANK() OVER (PARTITION BY player_id ORDER BY event_date) AS login_rank, 
+        -- Corrected: Get the NEXT date, THEN compare it
+        LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) = (event_date + INTERVAL '1 day') AS next_date_qualify
+    FROM activity
+)
+SELECT 
+    ROUND(
+        COUNT(DISTINCT player_id) FILTER (WHERE next_date_qualify)::DECIMAL 
+        / COUNT(DISTINCT player_id), 
+        2
+    ) AS fraction
+FROM temp
+WHERE login_rank = 1;
