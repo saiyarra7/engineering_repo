53117409d2a50b120cded57d384a62ecb829e8ec
saiyarra7 <saiyarra7@gmail.com>
2026-01-13 14:49:21 -0600

    prompt engineering setup

--- coding_practice/leetcode_sql_problems.sql
+++ coding_practice/leetcode_sql_problems.sql
@@ -155,3 +155,6 @@ select activity_date as day, count(distinct user_id) active_users
 from activity
 where activity_date > ('2019-07-27'::date - interval '30 day') and activity_date<= '2019-07-27'
 group by activity_date;
+
+
+
