a0de873caacbb3598c890c0a5a0ac90c62fe508e
saiyarra7 <saiyarra7@gmail.com>
2026-07-19 19:46:49 -0500

    local llm setup

---
+++ AI_engineering/setup_readme.py
@@ -0,0 +1,11 @@
+# Open powershell and run this command irm https://ollama.com/install.ps1 | iex
+# Download this extension in vscode - Continue - open-source AI code agent
+# run this "ollama run qwen3:8b" in powershell - This model is super slow and basically not usable in my laptop
+# Cntrl+D to stop the model from running
+# ollama stop qwen3:8b to unload from memory
+# ollama ps - to check which model is running
+# ollama run phi3 - fastest model for local code generation?  
+# ollama list
+# ollama rm qwen2.5:8b
+# ollama list
+
