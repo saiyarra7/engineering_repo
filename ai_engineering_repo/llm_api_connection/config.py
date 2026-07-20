0b8971817984f3d89323dfe4defc0f67a7424aa7
saiyarra7 <saiyarra7@gmail.com>
2026-07-20 17:27:03 -0500

    api key setup

---
+++ ai_engineering_repo/llm_api_connection/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+print(OPENAI_API_KEY)
+
